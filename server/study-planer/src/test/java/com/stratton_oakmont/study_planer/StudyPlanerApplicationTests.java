1ca25c7173740a8dc49d3450acf5655a628a2103
Nikolas Lethaus <nikolas.lethaus@gmail.com>
2025-05-22 21:18:39 +0200

    springboot gradle groovy setup and init

---
+++ server/study-planer/src/test/java/com/stratton_oakmont/study_planer/StudyPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stratton_oakmont.study_planer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
