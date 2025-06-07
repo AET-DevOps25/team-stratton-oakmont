0f66f37d98cc3c3e7e137ba8da9e52c4fd4296e4
Nikolas Lethaus <nikolas.lethaus@gmail.com>
2025-06-07 16:22:19 +0200

    init user-auth-service implemented

---
+++ server/user-auth-service/src/test/java/com/stratton_oakmont/ai_advisor_service/AiAdvisorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stratton_oakmont.ai_advisor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAdvisorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
