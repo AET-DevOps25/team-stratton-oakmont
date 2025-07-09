33e34b4a29cb88cd0c2ac87cbd144d1836838744
Florian Seitz <f.seitz@tum.de>
2025-07-09 18:02:43 +0200

    Update development API URLs to include versioning for study plan and user authentication

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -3,10 +3,10 @@ export const STUDY_PLAN_API_URL = window.location.hostname.includes(
   "tum-study-planner.student.k8s.aet.cit.tum.de"
 )
   ? "/api/study-plan" // Production
-  : "http://localhost:8081"; // Development
+  : "http://localhost:8081/api/v1"; // Development
 
 export const AUTH_API_URL = window.location.hostname.includes(
   "tum-study-planner.student.k8s.aet.cit.tum.de"
 )
   ? "/api/user-auth" // Production
-  : "http://localhost:8083"; // Development
+  : "http://localhost:8083/api/v1"; // Development
