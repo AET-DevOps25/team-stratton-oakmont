710ad63fa45dfd7558b7f638373333c8c12dd0ca
Florian Seitz <f.seitz@tum.de>
2025-07-11 15:49:31 +0200

    Refactor StudyPlanDatabaseConfig to use @Value for database properties and update Hibernate ddl-auto setting to create-drop

--- server/study-plan-service/src/main/java/com/stratton_oakmont/study_planer/config/StudyPlanDatabaseConfig.java
+++ server/study-plan-service/src/main/java/com/stratton_oakmont/study_planer/config/StudyPlanDatabaseConfig.java
@@ -1,7 +1,6 @@
 package com.stratton_oakmont.study_planer.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -10,29 +9,38 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 
 import javax.sql.DataSource;
 import jakarta.persistence.EntityManagerFactory;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(
-    basePackages = "com.stratton_oakmont.study_planer.repository",
-    excludeFilters = @ComponentScan.Filter(
-        type = FilterType.REGEX,
-        pattern = "com\\.stratton_oakmont\\.study_planer\\.repository\\.studydata\\..*"
-    ),
+    basePackages = "com.stratton_oakmont.study_planer.repository.studyplan",
     entityManagerFactoryRef = "studyPlanEntityManagerFactory",
     transactionManagerRef = "studyPlanTransactionManager"
 )
 public class StudyPlanDatabaseConfig {
-
+    
+    @Value("${DB_STUDY_PLAN_URL}")
+    private String url;
+    
+    @Value("${DB_STUDY_PLAN_USERNAME}")
+    private String username;
+    
+    @Value("${DB_STUDY_PLAN_PASSWORD}")
+    private String password;
+    
     @Primary
     @Bean(name = "studyPlanDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource studyPlanDataSource() {
-        return DataSourceBuilder.create().build();
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName("org.postgresql.Driver")
+                .build();
     }
 
     @Primary
@@ -40,8 +48,17 @@ public class StudyPlanDatabaseConfig {
     public LocalContainerEntityManagerFactoryBean studyPlanEntityManagerFactory() {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(studyPlanDataSource());
-        em.setPackagesToScan("com.stratton_oakmont.study_planer.entity");
-        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setPackagesToScan("com.stratton_oakmont.study_planer.entity.studyplan");
+        
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.show_sql", "true");
+        em.setJpaProperties(properties);
+        
         return em;
     }
 
